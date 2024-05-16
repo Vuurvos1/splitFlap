74e7fba74e8f3babeff07f075968c3cbfccdf9c2
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2024-05-16 08:32:25 +0200

    setup web server

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,49 +1,11 @@
 #include <Arduino.h>
 #include <config.h>
 
-// #include <WiFi.h>
-// #include <WebServer.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
 
-// SSID and password of Wifi connection:
-
-// IPAddress staticIP(192, 168, 1, 135);
-// IPAddress gateway(192, 168, 1, 1);
-// IPAddress subnet(255, 255, 255, 0);
-
-// WebServer server(80);
-
-// void handleRoot()
-// {
-//   server.send(200, "text/plain", "Hello, world!");
-// }
-
-// void setup()
-// {
-//   Serial.begin(115200);
-
-//   WiFi.config(staticIP, gateway, subnet);
-//   WiFi.begin(ssid, password);
-//   Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));
-
-//   // Wait for the WiFi connection to be established
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     delay(1000);
-//     Serial.print(".");
-//   }
-
-//   Serial.print("Connected to network with IP address: ");
-//   Serial.println(WiFi.localIP());
-
-//   server.on("/", handleRoot);
-
-//   server.begin();
-// }
-
-// void loop()
-// {
-//   server.handleClient();
-// }
+WebServer server(80);
 
 // 200 steps per revolution
 // 1 revolution = 360 degrees
@@ -83,16 +45,29 @@ void nextFlap(int delayTime = 500)
   step(steps, delayTime);
 }
 
+void handleRoot()
+{
+  server.send(200, "text/html", "<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\"></head><body><h1>Hey there!</h1></body></html>");
+}
+
+void handleNotFound()
+{
+  server.send(404, "text/plain", "Not found");
+}
+
 void setup()
 {
   Serial.begin(115200);
+
+  while (!Serial)
+    ;
 
   // Sets the two pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));
+  Serial.println("Establishing connection to WiFi");
 
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -101,31 +76,50 @@ void setup()
   }
   Serial.print("Connected to network with IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Initialize mDNS
+  if (!MDNS.begin("splitflap"))
+  { // Set the hostname to "splitflap.local"
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
+  server.on("/", handleRoot);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+
+  digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
 }
 
 void loop()
 {
-  digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
+  server.handleClient();
 
-  turnDeg(90, 500);
+  // turnDeg(90, 500);
 
-  delay(1500);
+  // delay(1500);
 
-  turnDeg(90, 300);
+  // turnDeg(90, 300);
 
-  delay(1500);
+  // delay(1500);
 
-  turnDeg(180, 500);
+  // turnDeg(180, 500);
 
-  for (int i = 0; i < 12; i++)
-  {
-    nextFlap(300);
-    delay(1000);
-  }
+  // for (int i = 0; i < 12; i++)
+  // {
+  //   nextFlap(300);
+  //   delay(1000);
+  // }
 
-  turnDeg(360 * 3, 300);
+  // turnDeg(360 * 3, 300);
 
-  delay(3000);
+  // delay(3000);
 
   // digitalWrite(dirPin, LOW); // Changes the rotations direction
   // // Makes 400 pulses for making two full cycle rotation
@@ -138,234 +132,3 @@ void loop()
   // }
   // delay(1000);
 }
-
-// WebServer server(80);
-
-// String webpage = "<!DOCTYPE html><html><head><title>Page Title</title></head><body style='background-color:#eee'><span style='color:#036'><h1>This is a Heading</h1><p>This is a paragraph.</p></span></body></html>";
-
-// void setup()
-// {
-//   Serial.begin(115200);
-
-//   WiFi.begin(ssid, password);
-//   Serial.println("Establishing connection to WiFi with SSID: " + String(ssid));
-
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     delay(1000);
-//     Serial.print(".");
-//   }
-//   Serial.print("Connected to network with IP address: ");
-//   Serial.println(WiFi.localIP());
-
-//   server.on("/", []()
-//             { server.send(200, "text\html", webpage); });
-
-//   server.begin();
-// }
-
-// void loop()
-// {
-//   server.handleClient();
-// }
-
-// /*********
-//   Rui Santos
-//   Complete project details at http://randomnerdtutorials.com
-// *********/
-
-// // Load Wi-Fi library
-// #include <WiFi.h>
-
-// // Replace with your network credentials
-// const char *ssid = "";
-// const char *password = "";
-
-// // // Set your Static IP address
-// // IPAddress local_IP(192, 168, 1, 184);
-// // // Set your Gateway IP address
-// // IPAddress gateway(192, 168, 1, 1);
-
-// // IPAddress subnet(255, 255, 0, 0);
-// // IPAddress primaryDNS(8, 8, 8, 8);   // optional
-// // IPAddress secondaryDNS(8, 8, 4, 4); // optional
-
-// // Set web server port number to 80
-// WiFiServer server(80);
-
-// // Variable to store the HTTP request
-// String header;
-
-// // Auxiliar variables to store the current output state
-// String output26State = "off";
-// String output27State = "off";
-
-// // Assign output variables to GPIO pins
-// const int output26 = 26;
-// const int output27 = 27;
-
-// // Current time
-// unsigned long currentTime = millis();
-// // Previous time
-// unsigned long previousTime = 0;
-// // Define timeout time in milliseconds (example: 2000ms = 2s)
-// const long timeoutTime = 2000;
-
-// void setup()
-// {
-//   Serial.begin(115200);
-//   // Initialize the output variables as outputs
-//   pinMode(output26, OUTPUT);
-//   pinMode(output27, OUTPUT);
-//   // Set outputs to LOW
-//   digitalWrite(output26, LOW);
-//   digitalWrite(output27, LOW);
-
-//   // // Configures static IP address
-//   // if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
-//   // {
-//   //   Serial.println("STA Failed to configure");
-//   // }
-
-//   // Connect to Wi-Fi network with SSID and password
-//   Serial.print("Connecting to ");
-//   Serial.println(ssid);
-//   WiFi.begin(ssid, password);
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     delay(500);
-//     Serial.print(".");
-//   }
-
-//   // Print local IP address and start web server
-//   Serial.println("");
-//   Serial.println("WiFi connected.");
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-//   server.begin();
-
-//   // Print ESP MAC Address
-//   Serial.println("MAC address: ");
-//   Serial.println(WiFi.macAddress());
-// }
-
-// void loop()
-// {
-//   WiFiClient client = server.available(); // Listen for incoming clients
-
-//   if (client)
-//   { // If a new client connects,
-//     currentTime = millis();
-//     previousTime = currentTime;
-//     Serial.println("New Client."); // print a message out in the serial port
-//     String currentLine = "";       // make a String to hold incoming data from the client
-//     while (client.connected() && currentTime - previousTime <= timeoutTime)
-//     { // loop while the client's connected
-//       currentTime = millis();
-//       if (client.available())
-//       {                         // if there's bytes to read from the client,
-//         char c = client.read(); // read a byte, then
-//         Serial.write(c);        // print it out the serial monitor
-//         header += c;
-//         if (c == '\n')
-//         { // if the byte is a newline character
-//           // if the current line is blank, you got two newline characters in a row.
-//           // that's the end of the client HTTP request, so send a response:
-//           if (currentLine.length() == 0)
-//           {
-//             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-//             // and a content-type so the client knows what's coming, then a blank line:
-//             client.println("HTTP/1.1 200 OK");
-//             client.println("Content-type:text/html");
-//             client.println("Connection: close");
-//             client.println();
-
-//             // turns the GPIOs on and off
-//             if (header.indexOf("GET /26/on") >= 0)
-//             {
-//               Serial.println("GPIO 26 on");
-//               output26State = "on";
-//               digitalWrite(output26, HIGH);
-//             }
-//             else if (header.indexOf("GET /26/off") >= 0)
-//             {
-//               Serial.println("GPIO 26 off");
-//               output26State = "off";
-//               digitalWrite(output26, LOW);
-//             }
-//             else if (header.indexOf("GET /27/on") >= 0)
-//             {
-//               Serial.println("GPIO 27 on");
-//               output27State = "on";
-//               digitalWrite(output27, HIGH);
-//             }
-//             else if (header.indexOf("GET /27/off") >= 0)
-//             {
-//               Serial.println("GPIO 27 off");
-//               output27State = "off";
-//               digitalWrite(output27, LOW);
-//             }
-
-//             // Display the HTML web page
-//             client.println("<!DOCTYPE html><html>");
-//             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-//             client.println("<link rel=\"icon\" href=\"data:,\">");
-//             // CSS to style the on/off buttons
-//             // Feel free to change the background-color and font-size attributes to fit your preferences
-//             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-//             client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-//             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-//             client.println(".button2 {background-color: #555555;}</style></head>");
-
-//             // Web Page Heading
-//             client.println("<body><h1>ESP32 Web Server</h1>");
-
-//             // Display current state, and ON/OFF buttons for GPIO 26
-//             client.println("<p>GPIO 26 - State " + output26State + "</p>");
-//             // If the output26State is off, it displays the ON button
-//             if (output26State == "off")
-//             {
-//               client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-//             }
-//             else
-//             {
-//               client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-//             }
-
-//             // Display current state, and ON/OFF buttons for GPIO 27
-//             client.println("<p>GPIO 27 - State " + output27State + "</p>");
-//             // If the output27State is off, it displays the ON button
-//             if (output27State == "off")
-//             {
-//               client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-//             }
-//             else
-//             {
-//               client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-//             }
-//             client.println("</body></html>");
-
-//             // The HTTP response ends with another blank line
-//             client.println();
-//             // Break out of the while loop
-//             break;
-//           }
-//           else
-//           { // if you got a newline, then clear currentLine
-//             currentLine = "";
-//           }
-//         }
-//         else if (c != '\r')
-//         {                   // if you got anything else but a carriage return character,
-//           currentLine += c; // add it to the end of the currentLine
-//         }
-//       }
-//     }
-//     // Clear the header variable
-//     header = "";
-//     // Close the connection
-//     client.stop();
-//     Serial.println("Client disconnected.");
-//     Serial.println("");
-//   }
-// }
