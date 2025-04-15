003cea6f71bbfa714b7a26affb2411a82dff5b2d
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2025-04-15 08:32:21 +0200

    add flap code to module

--- code/module/src/main.cpp
+++ code/module/src/main.cpp
@@ -1,19 +1,28 @@
 #include <Arduino.h>
 #include "utils.h"
 #include <vector>
+#include "SplitFlap.h"
 
 #define TXD1 19
 #define RXD1 21
 
+// defines pins
+#define STEP_PIN 13
+#define DIR_PIN 12
+
+SplitFlap splitFlap(STEP_PIN, DIR_PIN);
+
 // Use Serial1 for UART communication
 HardwareSerial mySerial(2);
+
+SplitFlap splitFlap(13, 12);
 
 bool readingPacket = false;
 
 void handleMyByte(uint8_t byte)
 {
   Serial.println("Received byte: " + String(byte));
-  // TODO: set the flap number
+  splitFlap.setFlap(byte);
 }
 
 void setup()
@@ -21,11 +30,15 @@ void setup()
   Serial.begin(115200);
   mySerial.begin(9600, SERIAL_8N1, RXD1, TXD1); // UART setup
 
+  splitFlap.init();
+
   Serial.println("Module started");
 }
 
 void loop()
 {
+  splitFlap.update();
+
   // Check if data is available to read
   if (mySerial.available())
   {
