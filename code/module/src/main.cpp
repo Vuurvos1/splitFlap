b82e2e063fb306a7ccb37539d5a205d92432daa4
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2025-04-22 08:31:04 +0200

    basic homing and testing logic

--- code/module/src/main.cpp
+++ code/module/src/main.cpp
@@ -7,15 +7,14 @@
 #define RXD1 21
 
 // defines pins
-#define STEP_PIN 13
-#define DIR_PIN 12
+#define STEP_PIN 22
+#define DIR_PIN 23
+#define HALL_PIN 15
 
-SplitFlap splitFlap(STEP_PIN, DIR_PIN);
+SplitFlap splitFlap(STEP_PIN, DIR_PIN, HALL_PIN);
 
 // Use Serial1 for UART communication
 HardwareSerial mySerial(2);
-
-SplitFlap splitFlap(13, 12);
 
 bool readingPacket = false;
 
@@ -31,6 +30,7 @@ void setup()
   mySerial.begin(9600, SERIAL_8N1, RXD1, TXD1); // UART setup
 
   splitFlap.init();
+  splitFlap.home();
 
   Serial.println("Module started");
 }
@@ -38,6 +38,14 @@ void setup()
 void loop()
 {
   splitFlap.update();
+
+  // move flap every 200ms
+  static unsigned long lastMove = 0;
+  if (millis() - lastMove > 5000)
+  {
+    splitFlap.moveFlaps(49); // TODO: make sure this also works with > 50
+    lastMove = millis();
+  }
 
   // Check if data is available to read
   if (mySerial.available())
--- code/shared/include/SplitFlap.h
+++ code/shared/include/SplitFlap.h
@@ -13,39 +13,84 @@
 class SplitFlap
 {
 public:
-  SplitFlap(int stepPin, int dirPin)
+  SplitFlap(int stepPin, int dirPin, int hallPin)
   {
     this->stepPin = stepPin;
     this->dirPin = dirPin;
+    this->hallPin = hallPin;
     stepper = AccelStepper(AccelStepper::DRIVER, stepPin, dirPin);
 
-    // Set the maximum speed and acceleration
-    stepper.setMaxSpeed(STEPS_PER_REVOLUTION * 8); // Set the maximum speed in steps per second
-    stepper.setAcceleration(STEPS_PER_REVOLUTION); // Set the acceleration in steps per second squared
+    stepper.setMaxSpeed(STEPS_PER_REVOLUTION * 2);
+    stepper.setAcceleration(STEPS_PER_REVOLUTION * 2);
+
+    lastHallState = digitalRead(hallPin);
+    lastPosition = 0;
   }
 
   void init()
   {
     pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
+    pinMode(hallPin, INPUT_PULLUP);
 
     digitalWrite(dirPin, HIGH); // Enables the motor to move in a particular direction
-
-    this->home();
   }
 
   void home()
   {
-    // TODO: implement homing sequence
+    // TODO: make this non blocking by adding module states
+    Serial.println("Homing");
+    stepper.setSpeed(1000); // Steps per second
+    while (digitalRead(hallPin) == HIGH)
+    {
+      stepper.runSpeed();
+    }
+    stepper.setCurrentPosition(0);
+
+    // TODO: add max steps
   }
 
   void update()
   {
-    // if (current pos % flapSteps == 0) {
-    //   stepper.stop(); // reset to prevent overflow
-    // }
-
     stepper.run();
+
+    // Check for home position passing
+    bool currentHallState = digitalRead(hallPin);
+    if (currentHallState != lastHallState)
+    {
+      if (currentHallState == LOW) // Hall sensor just triggered
+      {
+        int currentPosition = stepper.currentPosition();
+        int stepsSinceLastHome = currentPosition - lastPosition;
+
+        // Calculate how far we are from the nearest flap boundary
+        int positionError = currentPosition % FLAP_STEPS;
+        // If we're more than halfway through a flap, consider it as being close to the next boundary
+        if (positionError > FLAP_STEPS / 2)
+        {
+          positionError = FLAP_STEPS - positionError;
+        }
+
+        // Allow for more lenient tolerance (1/4 of a flap)
+        const int MAX_ALLOWED_ERROR = FLAP_STEPS / 4;
+
+        if (positionError > MAX_ALLOWED_ERROR)
+        {
+          Serial.printf("Warning: Position error detected! Off by %d steps (max allowed: %d)\n",
+                        positionError, MAX_ALLOWED_ERROR);
+          // Try to correct the position to the nearest flap boundary
+          int correctedPosition = (currentPosition / FLAP_STEPS) * FLAP_STEPS;
+          if (positionError > FLAP_STEPS / 2)
+          {
+            correctedPosition += FLAP_STEPS;
+          }
+          stepper.setCurrentPosition(correctedPosition);
+        }
+
+        lastPosition = currentPosition;
+      }
+      lastHallState = currentHallState;
+    }
   }
 
   void setCharacter(char c)
@@ -75,46 +120,25 @@ public:
     stepper.moveTo(currentPosition + steps * FLAP_STEPS);
   }
 
+  /** move x amount of flaps */
   void moveFlaps(int steps)
   {
-    stepper.moveTo(stepper.currentPosition() + steps * FLAP_STEPS);
+    // TODO: update to take the closest target position, instead of just adding steps
+    Serial.printf("Moving %d flaps to index %d\n", steps, (getFlapIndex() + steps) % FLAP_COUNT);
+    stepper.moveTo(stepper.targetPosition() + steps * FLAP_STEPS);
   }
 
 private:
   int stepPin;
   int dirPin;
+  int hallPin;
   AccelStepper stepper;
 
-  // 26 ABCDEFGHIJKLMNOPQRSTUVWXYZ
-  // 10 0123456789
-  // 20 !@#$%^&*()_+{}|:<>?/.,;[]\\=-`~
-  // 1 █ (full block)
-  // 1 space
+  bool lastHallState;
+  int lastPosition;
 
-  // "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!@#$%^&*()+:/.,=-`\" █
-  // ",.?!=/-+:$%()"
-
-  // ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}|:<>?/.,;[]\\=-`~"
-  // █
-
-  // const String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!@#$%^&*()+:/.,=-`\" █";
-  // "+-*!.#%@?,&': █"
-  // "?!@&()+-%$█*= "
-  // " █?!@%'+-=.,#:"
-
-  // TODO: maybe reuse 0 and o and 1 and i
-  // const String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789?!@#$%^&*()+:/.,=-`\" █";
-  // | is full block
-  // ?!@-=+&%$#.,:'
-
-  // "+-*!.#%@?,&': █"
-  // "?!@&()+-%$█*= "
-  // " █?!@%'+-=.,#:"
-
-  // +-:,.'%$@?!# █
-
-  const String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-:,.'%$@?!# |";
-  // | is full block??
+  // TOOD: update character list
+  const String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-:,.'%$@?!# █";
   const int FLAP_COUNT = characters.length();
   const int FLAP_STEPS = STEPS_PER_REVOLUTION / characters.length();
 
@@ -129,8 +153,11 @@ private:
     return index;
   }
 
+  // TODO: Get current flap index
   int getFlapIndex()
   {
     return stepper.currentPosition() / FLAP_STEPS % FLAP_COUNT;
   }
+
+  // TODO: get target flap index
 };
